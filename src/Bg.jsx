afcce85eb60ddd345e658c29206f4abf04d55ea4
irenap89 <irenap89@gmail.com>
2024-04-17 19:03:42 +0300

    lesson 2

--- src/Bg.jsx
+++ src/Bg.jsx
@@ -1,11 +1,56 @@
 
 import './Bg.css';
 import close_x from './assets/close.png'
+
+import logo from './assets/logo.png'
+import banner from './assets/banner.png'
+
+
+import Download_img from './Download_img'
+
 function Bg() {
   return (
     <div className='bg_general'>
         <img src={close_x} className='close_img'/>
          <div className='title'> העלאת תמונה כדי להסיר את הרקע</div>   
+
+         <button className='upload_btn'>העלאת תמונה  </button>
+         <div className='upload_text'> פורמטים נתמכים png, jpeg</div>
+
+
+         <div className='middle_div'>
+            <div className='right_div'>
+                <div className='right_div_inner'>
+                      <Download_img title="תמונה חינם" subtitle="תצוגה מקדימה של תמונה" top="true"></Download_img>
+
+                      <Download_img title="Pro" subtitle="תמונה מלאה" top="false" ></Download_img>
+
+                </div>
+            </div>
+
+            <div className='left_div'>
+              
+              <div className='left_div_inner'>
+
+              </div>
+
+              <div className='left_div_footer'>
+                  <button className='takanon_btn'> תקנון החברה </button>
+                  <div className='takanon_text'> על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות שלנו. אתר זה מוגן וחלים מדיניות ופרטיות </div>
+              </div>
+
+            </div>
+
+
+         </div>
+
+        <div className='footer_cont'>
+            <img src={logo} className='footer_logo'/>
+
+            <img src={banner}  className='footer_banner'/>
+
+        </div>
+
     </div>
   );
 }
---
+++ src/Download_img.jsx
@@ -0,0 +1,17 @@
+
+import './Download_img.css';
+import new_img from './assets/new.png' 
+
+function Download_img(props) {
+  return (
+    <div className='download_img_cont'>
+        <div  className={"download_img_title " + (props.top=="true" ? " padding_top_title " : '')}  >{props.title} </div>
+
+        { props.top=="false" ? <img src={new_img} className='download_img_new'/> : <></> }
+
+        <div className='download_img_subtitle'> {props.subtitle} </div>    
+    </div>
+);
+}
+
+export default Download_img;
