a72feb3207ab6a4d69e9af5083502ad29e61bf69
irenap89 <irenap89@gmail.com>
2024-05-01 19:01:28 +0300

    lesson 5

--- src/Bg.jsx
+++ src/Bg.jsx
@@ -4,13 +4,14 @@ import close_x from './assets/close.png'
 
 import logo from './assets/logo.png'
 import banner from './assets/banner.png'
-
+import axios from 'axios';
 
 import Download_img from './Download_img'
 import React, { useState } from 'react';
 import No_bg from './No_bg'
 import Download_popup from './Download_popup'
 import Eula from './Eula'
+import { useRef } from "react";
 
 function Bg() {
 
@@ -38,13 +39,47 @@ function Bg() {
     setshow_eula_popup(false);
   }
 
+
+  const inputElement = useRef();
+
+  const focusInput = () => {
+    inputElement.current.click();
+  };
+
+  function uploaded_file(e){
+
+     let file_info = e.target.files[0];
+
+
+     let url ='http://localhost:5000/upload_file';
+
+     let formData = new FormData();    //formdata object
+
+      formData.append('name', 'ABC');   //append the values with key, value pair
+      formData.append('age', 20);
+
+      const config = {     
+          headers: { 'content-type': 'multipart/form-data' }
+      }
+
+      axios.post(url, formData, config)
+      .then(response => {
+          console.log(response);
+      })
+      .catch(error => {
+          console.log(error);
+      });
+
+  }
+
   return (
     <>
     <div className='bg_general'>
         <img src={close_x} className='close_img'/>
          <div className='title'> העלאת תמונה כדי להסיר את הרקע</div>   
 
-         <button className='upload_btn'>העלאת תמונה  </button>
+         <button className='upload_btn' onClick={focusInput}>העלאת תמונה  </button>
+         <input type="file" ref={inputElement} className='input_file' onChange={uploaded_file}/>
          <div className='upload_text'> פורמטים נתמכים png, jpeg</div>
 
 
